d32c555685858f1343aaa88ebf54c93cd24cb4ac
nomad23541 <chrisreading.email@gmail.com>
2018-05-12 01:13:14 -0600

    Janitor work...

--- routes/media.js
+++ routes/media.js
@@ -1,47 +1,13 @@
 const metadata = require('../handlers/metadata')
 const config = require('../config')
 const fs = require('fs')
-const isVideo = require('is-video')
 const db = require('../utils/setup-db').db()
 const path = require('path')
-const getVideoInfo = require('get-video-info')
 
 module.exports = function(app) {
     app.get('/media/scanlibrary', function(req, res) { 
         db.remove({}, { multi: true })
         metadata.fetchMetadata()
-        /*
-        var files = fs.readdirSync(config.mediaDirectory)
-        // remove documents in the database that don't exist on the disk anymore
-        db.find({}, function(err, docs) {
-            // add file values in all documents to an array
-            var docFiles = []
-            for(var doc in docs) {
-                docFiles.push(docs[doc].file)
-            }
-            // find the difference between files and docFiles
-            var difference = docFiles.filter(x => !files.includes(x)).concat(files.filter(x => !docFiles.includes(x)))
-            // now remove all the differences in the database
-            for(var dif in difference) {
-                db.remove({ file: difference[dif] })
-            }
-        })
-
-        // now fetch the metadata for each file
-        files.forEach(file => {
-            // avoid duplicating, check the database to see if it already exists
-            db.findOne({ file: file }, function(err, doc) {
-                if(!doc) {
-                    // skip iteration if the file isn't a video
-                    if(!isVideo(file)) {
-                        return
-                    }
-                    // finally fetch the data
-                    metadata.fetchMetadata(file)
-                }
-            })
-        })
-        */
     })
 
     app.use('/media', function(req, res, next) {
