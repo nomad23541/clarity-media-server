9c6d3342ac35f79cea6a199d67ff7e596cddd8c8
nomad23541 <chrisreading.email@gmail.com>
2018-06-23 18:43:27 -0600

    Added option to show more actors on details page

--- public/scripts/details.js
+++ public/scripts/details.js
@@ -1,12 +1,44 @@
 $(document).ready(function() {
-    var playBtn = $('#playBtn')
-    var editBtn = $('#editBtn')
+    let btnPlay = $('#btnPlay')
+    let btnEdit = $('#btnEdit')
+    let btnShowMore = $('#btnShowMore')
+    let actors = $('#actors')
 
-    playBtn.click(function() {
+    btnPlay.click(function() {
         window.location.href = '/watch?id=' + id
     })
 
-    editBtn.click(function() {
+    btnEdit.click(function() {
         window.location.href = '/edit?id=' + id
     })
+
+    btnShowMore.click(function() {
+        getActors()
+        btnShowMore.hide()
+    })
+
+    // by default load 8 actors
+    getActors(8)
+
+    function getActors(amount) {
+        actors.empty()
+        $.getJSON('/api/media/' + id, function(data) {
+            // if amount isn't specified, get entire cast
+            if(!amount) {
+                amount = data.cast.length
+            }
+
+            for(let i = 0; i < amount; i++) {
+                let content = 
+                    '<div class="profile-box">' +
+                        '<a href="#">' +
+                            '<img src="' + data.cast[i].profileWeb + '" onerror="this.onerror=null;this.src=\'/img/poster-unknown.png\';">' +
+                            '<p class="profile-name">' + data.cast[i].name + '</p>' +
+                            '<p class="profile-role">as ' + data.cast[i].character + '</p>' +
+                        '</a>' +
+                    '</div>'
+                actors.append(content)
+            }
+        })
+    }
 })
