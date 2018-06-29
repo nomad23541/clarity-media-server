f78e96ca8226751bcc5b25d3c5b3237fd370ba06
nomad23541 <chrisreading.email@gmail.com>
2018-06-29 12:53:24 -0600

    Created routes for shows functionality

--- routes/library.js
+++ routes/library.js
@@ -10,15 +10,38 @@ module.exports = function(app) {
     })
 
     app.get('/library/shows', function(req, res) {
-        db.find({}, function(err, docs) {
-            res.render('movies/shows', {
+        db.find({ type: 'show' }, function(err, docs) {
+            res.render('shows/shows', {
                 size: docs.length
             })
         })
     })
 
+    app.get('/library/shows/show/:id/season/:season', function(req, res) {
+        db.findOne({ _id: req.params.id }, function(err, doc) {
+            if(err || !doc) return res.status(404).send('Not Found ')
+            // find season to use
+            let season = doc.seasons.find(season => season.seasonNumber == req.params.season)
+            db.find({ showName: doc.name, seasonNumber: parseInt(req.params.season), type: 'episode' }).sort({ episodeNumber: 1 }).exec(function(err, episodes) {
+                res.render('shows/season', {
+                    doc: doc,
+                    season: season,
+                    episodes: episodes
+                })
+            })
+        })
+    })
+
+    app.get('/library/shows/show', function(req, res) {
+        db.findOne({ _id: req.query.id }, function(err, doc) {
+            res.render('shows/show', {
+                doc: doc
+            })
+        })
+    })
+
     app.get('/library/movies', function(req, res) {
-        db.find({}, function(err, docs) {
+        db.find({ type: 'movie' }, function(err, docs) {
             res.render('movies/movies', {
                 size: docs.length
             })
@@ -36,7 +59,7 @@ module.exports = function(app) {
     app.get('/library/movies/movie', function(req, res) {
         let doc = req.body.doc
         // find movies that have the same genres
-        db.find({ genres: { $in: doc.genres }}, function(err, docs) {
+        db.find({ type: 'movie', genres: { $in: doc.genres }}, function(err, docs) {
             res.render('movies/movie',  {
                 doc: doc,
                 similar: docs.slice(0, 8) // only send 8 similar movies
@@ -55,7 +78,7 @@ module.exports = function(app) {
     app.get('/library/movies/similar', function(req, res) {
         let doc = req.body.doc
         // find movies that have the same genres
-        db.find({ genres: { $in: doc.genres }}, function(err, docs) {
+        db.find({ type: 'movie', genres: { $in: doc.genres }}, function(err, docs) {
             res.render('movies/similar',  {
                 doc: doc,
                 similar: docs
