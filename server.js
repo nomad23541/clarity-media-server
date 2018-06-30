cc884f41c6fec24b69e9963b1502c268abeb9d79
nomad23541 <chrisreading.email@gmail.com>
2018-06-29 22:22:11 -0600

    Create all directories needed in one function

--- server.js
+++ server.js
@@ -4,11 +4,14 @@ const http = require('http').Server(app)
 const config = require('./config')
 const fs = require('fs')
 const path = require('path')
+const setupDirectories = require('./lib/setup/setup-directories')
 const db = require('./lib/setup/setup-db')
 const socketIO = require('./lib/setup/setup-socket-io')
 const bodyParser = require('body-parser')
 const expressDevice = require('express-device')
 
+// create all directories needed in app
+setupDirectories.init()
 // setup socket io
 socketIO.init(http)
 // intialize database
@@ -18,8 +21,8 @@ app.set('view engine', 'ejs')
 app.use(bodyParser.json())
 app.use(expressDevice.capture())    
 app.use(express.static(path.join(__dirname, 'public')))
-app.use('/posters', express.static(config.posterDirectory))
-app.use('/profiles', express.static(path.join(config.posterDirectory, '/profiles')))
+app.use('/images', express.static(config.imagesDirectory))
+app.use('/profiles', express.static(path.join(config.imagesDirectory, '/profiles')))
 // use scripts in the node_modules in a view
 app.use('/scripts_server', express.static(path.join(__dirname, 'node_modules')))
 // set favicon
